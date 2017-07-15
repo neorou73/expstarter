8a16995c20d59fd2986d796000ab241b111af94f
Kris Poedjosoedarmo <neorou@gmail.com>
2017-07-15 17:52:02 -0400

    updates

--- index.js
+++ index.js
@@ -41,9 +41,41 @@ app.get('/example/:exampleType', function(req, res) {
   } else {
     res.status(400).json({ error: 'invalid request' });
   }
-
 });
 
+// create a new user account
+/*
+to test this, you can issue the following in a POSIX compliant terminal with curl installed
+curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:3000/api/login
+curl -H "Content-Type: application/json" -X POST -d 'sampleData' http://localhost:3000/create-new-user
+
+sampleData:
+{ "userData": {
+    "id"" "1e2c15f9-2d96-47b1-8f59-d2c36d5079b4",
+    "email": "someaddress@somedomain.com",
+    "username": "theTank",
+    "name": "Frank Ricard",
+    "password":"cumon!"
+  }
+}
+
+
+
+SO....
+
+curl -H "Content-Type: application/json" -X POST -d '{  "userData": {"id": "1e2c15f9-2d96-47b1-8f59-d2c36d5079b4", "email": "someaddress@somedomain.com", "username": "theTank",  "name": "Frank Ricard",  "password":"cumon!"} }' http://localhost:8000/create-new-user
+*/
+
+app.post('/create-new-user', function (req, res) {
+  if (!req.params.hasOwnProperty(userData)) {
+    res.status(400).json({ error: 'improper user data request - please specify user data' });
+  } else {
+    console.log(req.params);
+    res.send('user data recorded. thank you...');
+  }
+});
+
+// specify the app
 if (sm.config.hasOwnProperty('port')) {
   app.listen(sm.config.port, function () {
     console.log('Example app listening on ' + sm.config.host + ', port ' + sm.config.port + '!');
